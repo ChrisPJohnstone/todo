566a5104a4352a2ee06c37e28d37b1f7728a6f1c
Christopher Johnstone <chrisj97@proton.me>
2025-04-22 14:27:25 +0100

    Fixed completion dupes

--- completions/completion.bash
+++ completions/completion.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 function compare_array() {
-    echo "Not Implemented"
+    read -ra ARRAY1 <<< $1
+    read -ra ARRAY2 <<< $2
+    declare -a ARRAY3=()
+    for i in "${ARRAY1[@]}"; do
+        if ! printf '%s\0' "${ARRAY2[@]}" | grep -Fxqz -- $i; then
+            ARRAY3+=($i)
+        fi
+    done
+    echo ${ARRAY3[@]}
 }
 
 function completion_todo() {
@@ -25,10 +33,11 @@ function completion_todo() {
 
     local COMMAND="${COMP_WORDS[1]}"
     if [[ ${COMMAND} == "add" ]]; then
-        # TODO: Prevent duplicates
-        COMPREPLY=($(compgen -W "${CREATE_OPTS[*]}" -- "${COMP_WORDS[2]}"))
+        MISSING_OPTS=($(compare_array "${CREATE_OPTS[*]}" "${COMP_WORDS[*]}"))
+        COMPREPLY=($(compgen -W "${MISSING_OPTS[*]}"))
         return
     fi
+    # TODO: Above doesn't work very well for multiple options (`td add --opt1<tab>` becomes `td add --opt2<tab>`)
     # TODO: All the other commands
 }
 
