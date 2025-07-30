f3c16bdc93c2619060b7448e66cd684dce388212
Christopher Johnstone <chrisj97@proton.me>
2025-07-30 23:05:16 +0100

    Updated docstring for test util

--- src/test_utils/parametrize.py
+++ src/test_utils/parametrize.py
@@ -9,6 +9,14 @@ def parametrize(test_cases: TestSet) -> MarkDecorator:
     """
     Replaces the `@pytest.mark.parametrize` decorator to allow for a more
         readable test case definition.
+
+    Args:
+        test_cases (dict[str, dict[str, Any]]): A dictionary where each key is a test case name, and each value is a dictionary of parameters for that test case.
+        The keys of the inner dictionaries are the parameter names, and the values are the parameter values.
+
+    Returns:
+        MarkDecorator: A pytest mark decorator that can be used to parametrize tests.
+
     Example:
         cases: dict[str, dict[str, Any]] = {
             "positive": {"x": 1, "y": 2, "expected": 3},
