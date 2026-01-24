d6803476d6b5bac06f8a10c5ac4cc4ba586b1a61
Christopher Johnstone <chrisj97@proton.me>
2026-01-24 21:16:14 +0000

    Added app name and expiry time

--- src/services/notification/_linux.py
+++ src/services/notification/_linux.py
@@ -1,5 +1,18 @@
 import subprocess
 
 
-def _send_notification_linux(message: str) -> None:
-    subprocess.run(["notify-send", message])
+def _send_notification_linux(
+    app_name: str,
+    expiry_time: int,
+    message: str,
+) -> None:
+    args: list[str] = [
+        "notify-send",
+        "--app-name",
+        app_name,
+        "--expire-time",
+        str(expiry_time * 1000),
+        message,
+    ]
+    subprocess.run(args)
+    # TODO: Maybe don't rely on `notify-send` existing
--- src/services/notification/service.py
+++ src/services/notification/service.py
@@ -1,16 +1,45 @@
 from collections.abc import Callable
+from typing import Final
 
 from ._linux import _send_notification_linux
 from constants import Platform
 from utils import operating_system
 
-type SendMethod = Callable[[str], None]
+type SendMethod = Callable[[str, int, str], None]
 
 
 class NotificationService:
-    SEND_METHODS: dict[Platform, SendMethod] = {
+    DEFAULT_APP_NAME: Final[str] = "todo"
+    DEFAULT_EXPIRY_TIME: Final[int] = 3
+    SEND_METHODS: Final[dict[Platform, SendMethod]] = {
         Platform.LINUX: _send_notification_linux,
     }
+
+    def __init__(
+        self,
+        app_name: str = DEFAULT_APP_NAME,
+        expiry_time: int = DEFAULT_EXPIRY_TIME,
+    ) -> None:
+        self.app_name = app_name
+        self.expiry_time = expiry_time
+
+    @property
+    def app_name(self) -> str:
+        """Application name for the notification."""
+        return self._app_name
+
+    @app_name.setter
+    def app_name(self, value: str) -> None:
+        self._app_name: str = value
+
+    @property
+    def expiry_time(self) -> int:
+        """Expiry time for the notification in seconds."""
+        return self._expiry_time
+
+    @expiry_time.setter
+    def expiry_time(self, value: int) -> None:
+        self._expiry_time: int = value
 
     @property
     def operating_system(self) -> Platform:
@@ -20,4 +49,8 @@ class NotificationService:
         return self._operating_system
 
     def send_nofification(self, message: str) -> None:
-        self.SEND_METHODS[self.operating_system](message)
+        self.SEND_METHODS[self.operating_system](
+            self.app_name,
+            self.expiry_time,
+            message,
+        )
