332e8e17fa724d407717bd11ef45637074536f92
Christopher Johnstone <chrisj97@proton.me>
2026-01-25 20:25:49 +0000

    Added daemon shell

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,3 +1,4 @@
+from .daemon import DaemonService
 from .database import DatabaseService
 from .notification import NotificationService
 from .schedule import ScheduleService
---
+++ src/services/daemon.py
@@ -0,0 +1,127 @@
+from atexit import register
+from pathlib import Path
+from signal import SIGTERM
+from sys import exit, stderr, stdin, stdout
+from time import sleep
+from typing import Final
+import os
+
+from .notification import NotificationService
+
+
+class DaemonService:
+    DEFAULT_PIDFILE: Final[Path] = Path("/tmp/todo/todo.pid")
+
+    def __init__(self, pidfile: Path = DEFAULT_PIDFILE) -> None:
+        self.pidfile = pidfile
+
+    @property
+    def pidfile(self) -> Path:
+        """Path to the pidfile."""
+        return self._pidfile
+
+    @pidfile.setter
+    def pidfile(self, value: Path) -> None:
+        value.parent.mkdir(parents=True, exist_ok=True)
+        self._pidfile: Path = value
+
+    @property
+    def pidfile_exists(self) -> bool:
+        """Boolean indicating if the pidfile exists."""
+        return self.pidfile.exists()
+
+    def delete_pidfile(self) -> None:
+        """Deleted pidfile"""
+        self.pidfile.unlink()
+
+    def pid_from_pidfile(self) -> int:
+        """Return the pid from pidfile."""
+        if not self.pidfile_exists:
+            raise FileNotFoundError(f"pidfile {self.pidfile} does not exist.")
+        pid: str | None = self.pidfile.read_text().strip()
+        if not pid:
+            raise ValueError(f"pidfile {self.pidfile} is empty.")
+        return int(pid)
+
+    def _start_fork(self) -> int:
+        try:
+            pid: int = os.fork()
+            if pid > 0:
+                exit(0)
+            return pid
+        except OSError as error:
+            stderr.write(f"fork #1 failed: {error}\n")
+            exit(1)
+
+    def daemonize(self) -> None:
+        """Deamonize class. UNIX double fork mechanism."""
+        # First Fork
+        pid: int = self._start_fork()
+
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # Second Fork
+        pid: int = self._start_fork()
+
+        # redirect standard file descriptors
+        stdout.flush()
+        stderr.flush()
+        si = open(os.devnull, "r")
+        so = open(os.devnull, "a+")
+        se = open(os.devnull, "a+")
+
+        os.dup2(si.fileno(), stdin.fileno())
+        os.dup2(so.fileno(), stdout.fileno())
+        os.dup2(se.fileno(), stderr.fileno())
+
+        # write pidfile
+        register(self.delete_pidfile)
+        self.pidfile.write_text(f"{os.getpid()}\n")
+
+    def start(self) -> None:
+        """Start the daemon."""
+        try:
+            pid: int = self.pid_from_pidfile()
+            message: str = f"{pid} already exists. Daemon already running?\n"
+            stderr.write(message)
+            exit(1)
+        except (FileNotFoundError, ValueError):
+            pass
+        self.daemonize()
+        self.run()
+
+    def stop(self) -> None:
+        """Stop the daemon."""
+        try:
+            pid: int = self.pid_from_pidfile()
+        except (FileNotFoundError, ValueError):
+            message: str = "pidfile does not exist. Daemon not running?\n"
+            stderr.write(message)
+            return
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                sleep(0.1)
+        except OSError as error:
+            error_str: str = str(error.args)
+            if error_str.find("No such process") > 0:
+                if self.pidfile_exists:
+                    self.delete_pidfile()
+            else:
+                print(error_str)
+                exit(1)
+
+    def restart(self) -> None:
+        """Restart the daemon."""
+        self.stop()
+        self.start()
+
+    def run(self) -> None:
+        """Daemon main loop."""
+        notifier: NotificationService = NotificationService()
+        while True:
+            sleep(1)
+            notifier.send_notification("test")
